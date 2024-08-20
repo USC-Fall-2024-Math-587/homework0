e26e3a24b7748d85ebe2dee9fd7fd7cb5a185796
Matthew Ballard <matt@mrb.email>
2024-08-20 11:15:33 -0400

    switch dependencies

--- lakefile.lean
+++ lakefile.lean
@@ -4,7 +4,7 @@ open Lake DSL
 package hw0 where
   testDriver := "batteries/test"
 
-require "leanprover-community" / "batteries" @ git "main"
+require USCMath587 from git "https://github.com/USC-Fall-2024-Math-587/USCMath587"@"main"
 
 lean_lib «Hw0» where
 
